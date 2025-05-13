986e7f8484f480364a10ce488f5ecd9f2a794a2b
labibahhoque <hoquelabibah@gmail.com>
2025-05-13 11:53:02 +0600

    second

--- server.js
+++ server.js
@@ -41,7 +41,10 @@ app.post("/create", (req, res) => {
     .then((user) => res.json(user))
     .catch((err) => console.log(err));
 });
-
+app.use(express.static("./client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`Server is running on post ${port}`);
 });
